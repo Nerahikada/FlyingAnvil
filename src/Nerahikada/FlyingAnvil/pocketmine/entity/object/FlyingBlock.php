237d93151ea1e600f0e0a66b05666f64dfb4d2aa
Nerahikada <hkshirorom@yahoo.co.jp>
2020-06-08 19:37:20 +0900

    神
    そんなことできるんですか！！！

--- src/Nerahikada/FlyingAnvil/pocketmine/entity/object/FlyingBlock.php
+++ src/Nerahikada/FlyingAnvil/pocketmine/entity/object/FlyingBlock.php
@@ -6,82 +6,22 @@ namespace Nerahikada\FlyingAnvil\pocketmine\entity\object;
 
 use Nerahikada\FlyingAnvil\utils\Math;
 use pocketmine\block\Block;
-use pocketmine\block\BlockFactory;
 use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\level\Explosion;
 use pocketmine\level\particle\MobSpawnParticle;
 use pocketmine\level\sound\AnvilFallSound;
-use pocketmine\nbt\tag\ByteTag;
-use pocketmine\nbt\tag\IntTag;
-use function get_class;
 
-/**
- * できれば \pocketmine\entity\object\FallingBlock を継承したかったが、
- * parent::entityBaseTick() を呼び出さなければいけないので断念
- *
- * TL;DR コピペなんてしたくねぇ💩
- */
-class FlyingBlock extends Entity{
-	public const NETWORK_ID = self::FALLING_BLOCK;
-
-	public $width = 0.98;
-	public $height = 0.98;
-
-	protected $baseOffset = 0.49;
+class FlyingBlock extends \pocketmine\entity\object\FallingBlock{
 
 	protected $gravity = -0.04;
-	protected $drag = 0.02;
-
-	/** @var Block */
-	protected $block;
-
-	public $canCollide = false;
-
-	protected function initEntity() : void{
-		parent::initEntity();
-
-		$blockId = 0;
-
-		//TODO: 1.8+ save format
-		if($this->namedtag->hasTag("TileID", IntTag::class)){
-			$blockId = $this->namedtag->getInt("TileID");
-		}elseif($this->namedtag->hasTag("Tile", ByteTag::class)){
-			$blockId = $this->namedtag->getByte("Tile");
-			$this->namedtag->removeTag("Tile");
-		}
-
-		if($blockId === 0){
-			throw new \UnexpectedValueException("Invalid " . get_class($this) . " entity: block ID is 0 or missing");
-		}
-
-		$damage = $this->namedtag->getByte("Data", 0);
-
-		$this->block = BlockFactory::get($blockId, $damage);
-
-		$this->propertyManager->setInt(self::DATA_VARIANT, $this->block->getRuntimeId());
-	}
-
-	public function canCollideWith(Entity $entity) : bool{
-		return false;
-	}
-
-	public function canBeMovedByCurrents() : bool{
-		return false;
-	}
-
-	public function attack(EntityDamageEvent $source) : void{
-		if($source->getCause() === EntityDamageEvent::CAUSE_VOID){
-			parent::attack($source);
-		}
-	}
 
 	public function entityBaseTick(int $tickDiff = 1) : bool{
 		if($this->closed){
 			return false;
 		}
 
-		$hasUpdate = parent::entityBaseTick($tickDiff);
+		$hasUpdate = Entity::entityBaseTick($tickDiff);
 
 		if($this->y >= 256 + 16 and $this->isAlive()){
 			$ev = new EntityDamageEvent($this, EntityDamageEvent::CAUSE_VOID, 10);
@@ -104,19 +44,5 @@ class FlyingBlock extends Entity{
 		}
 
 		return $hasUpdate;
-	}
-
-	public function getBlock() : int{
-		return $this->block->getId();
-	}
-
-	public function getDamage() : int{
-		return $this->block->getDamage();
-	}
-
-	public function saveNBT() : void{
-		parent::saveNBT();
-		$this->namedtag->setInt("TileID", $this->block->getId(), true);
-		$this->namedtag->setByte("Data", $this->block->getDamage());
 	}
 }
